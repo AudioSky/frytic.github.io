c361fce0697a65da5248546f77e118928064ecde
AudioSky <62019385+AudioSky@users.noreply.github.com>
2022-09-07 22:22:58 +0500

    2048:Balls
    2048Balls

---
+++ 2048/scripts/project/scriptsInEvents.js
@@ -0,0 +1,48 @@
+
+
+
+const scriptsInEvents = {
+
+	async Menusheet_balls_Event6_Act2(runtime, localVars)
+	{
+		 console.log("MySocial");
+		  vkBridge.send('VKWebAppShowWallPostBox',
+					   {"message": "Продержись как можно дольше!",
+			     	     "attachments": "https://vk.com/app51420108_587856911"})
+						  .then(data => {
+						     console.log(data);});	
+	},
+
+	async Menusheet_balls_Event7_Act2(runtime, localVars)
+	{
+		 vkBridge.send("VKWebAppShowInviteBox",                  {"method":"VKWebAppShowInviteBox","params":{}})
+		         .then(data => console.log(data.success))  
+		        .catch(error => console.log(error));	
+						console.log("tMyFrend");	
+	},
+
+	async Egame_balls_Event150_Act1(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	},
+
+	async Egame_balls_Event150_Act2(runtime, localVars)
+	{
+		vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+						 
+		function MyAdd(){
+		 vkBridge.send('VKWebAppShowNativeAds',{ad_format:"interstitial"}).then(data => {
+						 console.log(data);});	
+		}
+		MyAdd();
+	},
+
+	async Egame_balls_Event150_Act4(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
