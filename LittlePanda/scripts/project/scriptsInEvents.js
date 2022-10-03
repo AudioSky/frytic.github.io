15b0f69f4b4d64056b414ebaff60b77a87f22c8d
AudioSky <62019385+AudioSky@users.noreply.github.com>
2022-10-03 15:18:36 +0500

    Panda_Littlel

---
+++ LittlePanda/scripts/project/scriptsInEvents.js
@@ -0,0 +1,43 @@
+
+
+
+const scriptsInEvents = {
+
+	async Egame_Event225_Act1(runtime, localVars)
+	{
+		vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+						 
+		function MyAdd(){
+		 vkBridge.send('VKWebAppShowNativeAds',{ad_format:"interstitial"}).then(data => {
+						 console.log(data);});	
+		}
+		MyAdd();
+	},
+
+	async Egame_Event225_Act3(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	},
+
+	async Emenu_Event13_Act2(runtime, localVars)
+	{
+		  console.log("MySocial");
+		  vkBridge.send('VKWebAppShowWallPostBox',
+					   {"message": "Собери Три в Ряд!",
+			     	     "attachments": "https://vk.com/app51440258_587856911"})
+						  .then(data => {
+						     console.log(data);});	
+	},
+
+	async Emenu_Event14_Act2(runtime, localVars)
+	{
+		 vkBridge.send("VKWebAppShowInviteBox",                  {"method":"VKWebAppShowInviteBox","params":{}})
+		         .then(data => console.log(data.success))  
+		        .catch(error => console.log(error));	
+						console.log("tMyFrend");	
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
