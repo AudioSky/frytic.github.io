eb3e59149e422605e5151b6dc0eabb740a42bb6b
AudioSky <62019385+AudioSky@users.noreply.github.com>
2022-10-20 22:48:02 +0500

    2048:Cliker

---
+++ MergeNumber/scripts/project/scriptsInEvents.js
@@ -0,0 +1,43 @@
+
+
+
+const scriptsInEvents = {
+
+	async Gameplayevents_Event138_Act1(runtime, localVars)
+	{
+		vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+						 
+		function MyAdd(){
+		 vkBridge.send('VKWebAppShowNativeAds',{ad_format:"interstitial"}).then(data => {
+						 console.log(data);});	
+		}
+		MyAdd();
+	},
+
+	async Gameplayevents_Event138_Act3(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	},
+
+	async Gamehomeevents_Event10_Act2(runtime, localVars)
+	{
+		  console.log("MySocial");
+		  vkBridge.send('VKWebAppShowWallPostBox',
+					   {"message": "Продержись как можно дольше!",
+			     	     "attachments": "https://vk.com/app51453978_587856911"})
+						  .then(data => {
+						     console.log(data);});	
+	},
+
+	async Gamehomeevents_Event11_Act2(runtime, localVars)
+	{
+		 vkBridge.send("VKWebAppShowInviteBox",                  {"method":"VKWebAppShowInviteBox","params":{}})
+		         .then(data => console.log(data.success))  
+		        .catch(error => console.log(error));	
+						console.log("tMyFrend");	
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
