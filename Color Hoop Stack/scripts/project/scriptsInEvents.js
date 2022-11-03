6f025462497b9e4db8bf35c146c36fb7880c6fb8
AudioSky <62019385+AudioSky@users.noreply.github.com>
2022-11-03 14:31:55 +0500

    Color_Hoop

---
+++ Color Hoop Stack/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Win_Event106_Act1(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	},
+
+	async Win_Event106_Act4(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
