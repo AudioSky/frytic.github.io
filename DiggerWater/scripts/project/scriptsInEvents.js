a4a5d4927aea6a95664e9cbf1d95d3e598d71dea
AudioSky <62019385+AudioSky@users.noreply.github.com>
2022-12-14 19:53:40 +0500

    Digger_Water

---
+++ DiggerWater/scripts/project/scriptsInEvents.js
@@ -0,0 +1,59 @@
+
+
+
+const scriptsInEvents = {
+
+	async Action_Event34_Act1(runtime, localVars)
+	{
+		vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+						 
+		function MyAdd(){
+		 vkBridge.send('VKWebAppShowNativeAds',{ad_format:"interstitial"}).then(data => {
+						 console.log(data);});	
+		}
+		MyAdd();
+	},
+
+	async Action_Event34_Act3(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	},
+
+	async Btn_click_Event47_Act4(runtime, localVars)
+	{
+		 vkBridge.send("VKWebAppShowInviteBox",                  {"method":"VKWebAppShowInviteBox","params":{}})
+		         .then(data => console.log(data.success))  
+		        .catch(error => console.log(error));	
+						console.log("tMyFrend");	
+	},
+
+	async Btn_click_Event50_Act4(runtime, localVars)
+	{
+		  console.log("MySocial");
+		  vkBridge.send('VKWebAppShowWallPostBox',
+					   {"message": "Закати шарик!",
+			     	     "attachments": "https://vk.com/app8175283_587856911"})
+						  .then(data => {
+						     console.log(data);});	
+	},
+
+	async Actionwtr_Event34_Act1(runtime, localVars)
+	{
+		vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+						 
+		function MyAdd(){
+		 vkBridge.send('VKWebAppShowNativeAds',{ad_format:"interstitial"}).then(data => {
+						 console.log(data);});	
+		}
+		MyAdd();
+	},
+
+	async Actionwtr_Event34_Act3(runtime, localVars)
+	{
+		console.log(runtime.globalVars.Abd);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
