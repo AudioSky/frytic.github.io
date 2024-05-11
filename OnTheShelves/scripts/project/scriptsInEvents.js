b51c724a1e3c156893f95359f3d9a6c1ab91fb69
AudioSky <62019385+AudioSky@users.noreply.github.com>
2024-05-11 11:07:49 +0500

    on_the_svelas

---
+++ OnTheShelves/scripts/project/scriptsInEvents.js
@@ -0,0 +1,183 @@
+
+
+
+const scriptsInEvents = {
+
+	async Action_Event1_Act1(runtime, localVars)
+	{
+		vkBridge.send('VKWebAppShowBannerAd', {
+		
+		  banner_location: 'top',
+		  layout_type: 'resize'
+		
+		  })
+		 .then((data) => { 
+		    if (data.result) {
+		      // Баннерная реклама отобразилась
+		    }
+		  })
+		  .catch((error) => {
+		    // Ошибка
+		    console.log(error);
+		  });
+	},
+
+	async Action_Event571_Act2(runtime, localVars)
+	{
+		vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+		
+		// ...  
+		
+		// Показать рекламу
+		vkBridge.send("VKWebAppShowNativeAds", {"ad_format": "interstitial"}).then((data) => {
+		    if (data.result){
+		      console.log('Реклама показана');
+			  	runtime.globalVars.ReklSound=1;			
+				console.log(runtime.globalVars.ReklSound);
+			  }
+		    else{
+		      console.log('Ошибка при показе');	  	
+			  }
+		  })
+		  .catch((error) => { console.log(error); 
+		  
+		       runtime.globalVars.ReklSound=1;			
+				console.log(runtime.globalVars.ReklSound);/* Ошибка */ });
+	},
+
+	async Action_Event571_Act3(runtime, localVars)
+	{
+		console.log(runtime.globalVars.ReklSound);
+	},
+
+	async Action_Event579_Act2(runtime, localVars)
+	{
+		
+		YaGames
+		    .init()
+		    .then(ysdk => {
+		        ysdk.adv.showFullscreenAdv({
+		    callbacks: {
+			    onOpen: () => {
+		          console.log('showFullscreenAdv.');
+				  runtime.globalVars.ReklSound=3;
+				  console.log(runtime.globalVars.ReklSound);
+		        },
+				onClose: function(wasShown) {
+		           console.log('close.');
+				  runtime.globalVars.ReklSound=4;
+		        },	
+		        onError: function(error) {
+		            console.log('erro.');
+				  runtime.globalVars.ReklSound=4;
+		        },
+				onOffline: function() {
+		            console.log('onOffline.');
+				  runtime.globalVars.ReklSound=4;
+		        }
+		    }
+		   })
+		});
+		
+	},
+
+	async Action_Event586_Act1(runtime, localVars)
+	{
+		 ysdk.getLeaderboards()
+		  .then(lb => {
+		    // Без extraData
+		    lb.setLeaderboardScore('Best', runtime.globalVars.RecordScore);
+		  });
+		
+		
+		
+	},
+
+	async Butons_Event41_Act4(runtime, localVars)
+	{
+		vkBridge.send('VKWebAppCheckNativeAds', { ad_format: 'reward' })
+		  .then((data) => {
+		    if (data.result) {
+		
+		      // Предзагруженная реклама есть.
+		 
+		      // Теперь можно создать кнопку
+		      // "Прокачать героя за рекламу".   
+		      // ...
+		        fooButtonClick();    
+		      } else {
+		        console.log('Рекламные материалы не найдены.');
+		      }
+		  })
+		  .catch((error) => { console.log(error); /* Ошибка */  });
+		
+		// Обработчик нажатия кнопки "Посмотрите рекламу, чтобы прокачать героя"
+		function fooButtonClick()
+		{
+		  // Показать рекламу
+		vkBridge.send('VKWebAppShowNativeAds', { ad_format: 'reward' })
+		    .then((data) => {
+		      if (data.result) {// Успех
+			    console.log('Реклама показана');
+				
+				runtime.globalVars.PrizTwo=2;
+				console.log(runtime.globalVars.Priz); 
+				
+				runtime.globalVars.Priz=1;		 
+				console.log(runtime.globalVars.Priz); 
+				
+		      }else{ // Ошибка 
+		        console.log('Ошибка при показе');
+				}
+		    })
+		    .catch((error) => { 
+			console.log(error); 
+			
+			runtime.globalVars.Priz=1;		 
+			console.log(runtime.globalVars.Priz); 
+			/* Ошибка */ });
+		}
+	},
+
+	async Start_Event21_Act3(runtime, localVars)
+	{
+		 console.log("MySocial");
+		  vkBridge.send('VKWebAppShowWallPostBox',
+					   {"message": "Разложи По Полочкам!",
+			     	     "attachments": "https://vk.com/app51921437_587856911"})
+						  .then(data => {
+						     console.log(data);});	
+	},
+
+	async Start_Event22_Act3(runtime, localVars)
+	{
+		 vkBridge.send("VKWebAppShowInviteBox",   
+		       {"method":"VKWebAppShowInviteBox","params":{}})
+		         .then(data => console.log(data.success))  
+		        .catch(error => console.log(error));	
+						console.log("tMyFrend");	
+	},
+
+	async Choise_Event1_Act1(runtime, localVars)
+	{
+		vkBridge.send('VKWebAppShowBannerAd', {
+		
+		  banner_location: 'top',
+		  layout_type: 'resize'
+		
+		  })
+		 .then((data) => { 
+		    if (data.result) {
+		      // Баннерная реклама отобразилась
+		    }
+		  })
+		  .catch((error) => {
+		    // Ошибка
+		    console.log(error);
+		  });
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
